fa4f2ed7885c06547e6b64454cbdbb80a8961c1c
Lisa Rodriguez <lisa.rodriguez257@gmail.com>
2018-08-07 14:38:23 -0500

    added contacts generator

--- src/Test2.java
+++ src/Test2.java
@@ -1,5 +1,37 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Test2 {
+
 	public static void main(String[] args) {
-		System.out.println("Hello");
+
+//This creates the contacts file.
+		String directory = "data";
+		String filename = "contacts.txt";
+		Path dataDirectory = Paths.get(directory);
+		Path dataFile = Paths.get(directory, filename);
+
+		if (!Files.isDirectory(dataDirectory)){
+			try {
+				Files.createDirectories(dataDirectory);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		if (!Files.exists(dataFile)){
+			try {
+				Files.createFile(dataFile);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+
+
+
 	}
 }
